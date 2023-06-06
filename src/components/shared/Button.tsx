e627e73b14463c67a4e1132c60d8028a086de609
MaxDecapitars <mdecapitars@gmail.com>
2023-06-06 13:59:16 -0500

    Improving Buttons

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -4,12 +4,12 @@ type Props = {
   children?: React.ReactNode;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   type?: 'button' | 'reset' | 'submit';
-  title?: string
+  title?: string;
 };
 
 const Button = ({ children, onClick, type = 'button', title }: Props) => {
   const clipPath =
-    '[clip-path:_polygon(80%_0,_100%_30%,_100%_100%,_0_100%,_0_0)]';
+    '[clip-path:_polygon(calc(100%_-_.75rem)_0,_100%_.75rem,_100%_100%,_0_100%,_0_0)]';
   const gradient =
     'bg-[linear-gradient(120deg,_#fe80b1,_#960707,_#dfb548,_#7a5ee0,_#1E228F,_#5bfffa)] [background-size:_300%_100%]';
 
@@ -26,7 +26,8 @@ const Button = ({ children, onClick, type = 'button', title }: Props) => {
       whileTap={{ scale: 0.8 }}
     >
       <span
-        className={`flex h-full items-center justify-center rounded-md bg-white bg-opacity-80 p-1 uppercase ${clipPath} leading-none dark:bg-black dark:bg-opacity-70`}
+        className={`flex h-full items-center justify-center rounded-md bg-white bg-opacity-80 p-1 px-2
+         uppercase ${clipPath} leading-none dark:bg-black dark:bg-opacity-70`}
       >
         {children}
       </span>
