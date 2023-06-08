1c2dec95f7f1b1db4bd19e929948965eea361999
MaxDecapitars <mdecapitars@gmail.com>
2023-06-08 13:56:33 -0500

    Improved light theme

--- src/components/Background/BackgroundColor.tsx
+++ src/components/Background/BackgroundColor.tsx
@@ -1,6 +1,6 @@
 const BackgroundColor = () => {
   return (
-    <div className="absolute bottom-0 left-0 right-0 top-0 -z-50 dark:bg-secondary-950"></div>
+    <div className="absolute bottom-0 left-0 right-0 top-0 -z-50 bg-slate-50 transition-colors dark:bg-secondary-950"></div>
   );
 };
 export default BackgroundColor;
