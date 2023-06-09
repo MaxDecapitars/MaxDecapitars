e302d1ce4b70d8500e4990f134a8a74b7156ce79
MaxDecapitars <mdecapitars@gmail.com>
2023-06-08 22:24:45 -0500

    Improved Home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
 import SelectedPageContext from '@/context/SelectedPageContexts';
 import { SelectedPage } from '@/types/types';
-import { animationBottom } from '@/types/const';
+import { animationBottom, animationTop } from '@/types/const';
 import Section from '@/components/shared/Section';
 import Button from '../shared/Button';
 
@@ -20,38 +20,49 @@ const Home = () => {
 
   return (
     <Section id={SelectedPage.Home}>
-      <motion.h1
-        className="flex h-full flex-col justify-center gap-4 text-center"
+      <motion.div
+        className="flex flex-grow flex-col items-center justify-center text-center laptop:grid laptop:grid-rows-3"
         onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
-        {...animationBottom}
       >
-        <strong className="text-5xl tablet:text-7xl">Camilo Villarraga</strong>
-        <strong className="text-2xl dark:text-primary-50 tablet:text-3xl">
-          {title}
-        </strong>
-        <p className="text-primary-600 dark:text-primary-100">{slogan}</p>
-        <div className="mt-8 flex flex-row justify-center gap-4">
-          <a
-            href="https://github.com/MaxDecapitars"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-          <a
-            href="https://www.linkedin.com/in/camilo-villarraga/?locale=en_US"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Linkedin
-          </a>
-        </div>
-        <div>
-          <Button onClick={handleDownloadCV}>
-            <span className="p-2">{btnText}</span>
-          </Button>
-        </div>
-      </motion.h1>
+        <motion.h1
+          className="flex flex-col justify-center gap-4 text-center laptop:row-start-2 laptop:self-center"
+          {...animationBottom}
+        >
+          <strong className="text-5xl tablet:text-7xl">
+            Camilo Villarraga
+          </strong>
+          <strong className="text-2xl dark:text-primary-50 tablet:text-3xl">
+            {title}
+          </strong>
+          <p className="text-primary-600 dark:text-primary-100">{slogan}</p>
+        </motion.h1>
+        <motion.div
+          className="flex flex-col gap-4 laptop:row-start-3 laptop:self-end"
+          {...animationTop}
+        >
+          <div className="mt-8 flex flex-row justify-center gap-4">
+            <a
+              href="https://github.com/MaxDecapitars"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://www.linkedin.com/in/camilo-villarraga/?locale=en_US"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Linkedin
+            </a>
+          </div>
+          <div>
+            <Button onClick={handleDownloadCV}>
+              <span className="p-2">{btnText}</span>
+            </Button>
+          </div>
+        </motion.div>
+      </motion.div>
     </Section>
   );
 };
